dc892e87de5ef3a03969407b64b6b6e0ece76653
Hannah Osei <hannah.osei@softwire.com>
2025-04-24 14:21:21 +0100

    get arivals and sort by ascending times

--- busBoard.js
+++ busBoard.js
@@ -1,2 +1,20 @@
 import fetch from 'node-fetch';
 import {config} from 'dotenv';
+config();
+
+const API_KEY = process.env.API_KEY;
+const STOP_CODE = '490008660N';
+const url = `https://api.tfl.gov.uk/StopPoint/${STOP_CODE}/Arrivals?app_key=${API_KEY}`;
+//let sortedBusArrivals = {}
+
+async function getBusArrivals() {
+
+    const fetchBus = await fetch(url);
+    const busData = await fetchBus.json();
+    const sortedBusArrivals = busData.sort((a,b) => a.timeToStation - b.timeToStation);
+    
+    return sortedBusArrivals;
+};
+const sortedBusArrivals = await getBusArrivals();
+
+console.log(sortedBusArrivals);
