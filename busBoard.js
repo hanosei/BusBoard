00db9dc3b9bcbe978c590cee24bbd5a8951818f5
Alice Barrett <alice.barrett@softwire.com>
2025-04-25 12:23:52 +0100

    Cleaned up code

--- busBoard.js
+++ busBoard.js
@@ -5,46 +5,25 @@ config();
 const API_KEY = "d40a5d1e4ea34f13b25595a3ba3ee6d4";
 
 
-const postUrl = "https://api.postcodes.io/postcodes/nw51tl";
-
-async function getLongLat() {
-  const fetchLocation = await fetch(postUrl);
+async function getCoordinates() {
+  const fetchLocation = await fetch("https://api.postcodes.io/postcodes/nw51tl");
   const locationData = await fetchLocation.json();
-  const longLat = [];
-  await longLat.push(locationData.result.longitude);
-  await longLat.push(locationData.result.latitude);
+  const longLat = {longitude:locationData.result.longitude, latitude:locationData.result.latitude};
   return longLat;
 }
 
-const locationArray = await getLongLat();
+const coordinates = await getCoordinates();
 
-const url = `https://api.tfl.gov.uk/StopPoint/?lat=${locationArray[1]}&lon=${locationArray[0]}%20&stopTypes=NaptanPublicBusCoachTram%20&radius=500&modes=bus`;
-//let sortedBusArrivals = {}
-
-async function getBusStops() {
-  const fetchBus = await fetch(url);
+async function getNearestBusStops() {
+  const fetchBus = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${coordinates.latitude}&lon=${coordinates.longitude}%20&stopTypes=NaptanPublicBusCoachTram%20&radius=500&modes=bus`);
   const busData = await fetchBus.json();
-  return busData;
-}
-const sortedBusArrivals = await getBusStops();
-
-
-function getCloseBusStops(busData){
   let nearestBusStops = {};
-  //let busStopIDs = [];
   for (let i = 0; i < 2; i++) {
     nearestBusStops[busData.stopPoints[i].commonName] = busData.stopPoints[i].id;
   } 
   
-  // for (let i =0; i<busStopIDs.length-1; i++) {
-  //   getBusArrivals(busStopIDs[i],nearestBusStops[i]);
-  // }
   return nearestBusStops;
-  
 }
-
- 
-
 
 async function getBusArrivals(keys,values) {
   for (let i=0; i<keys.length;i++) {
@@ -60,7 +39,6 @@ async function getBusArrivals(keys,values) {
 
 function get5Buses(sortedBusArrivals) {
   
-
   for (let i = 0; i < 5; i++) {
     console.log("Number " + sortedBusArrivals[i].lineName);
     console.log("Towards " + sortedBusArrivals[i].towards);
@@ -70,14 +48,13 @@ function get5Buses(sortedBusArrivals) {
   }
 }
 
+const sortedBusArrivals = await getNearestBusStops();
+getBusArrivals(sortedBusArrivals);
 
-const nearestBusStops = getCloseBusStops(sortedBusArrivals);
-getBusArrivals(nearestBusStops);
+ const keys = Object.keys(sortedBusArrivals);
+ const values = Object.values(sortedBusArrivals);
 
- const keys = Object.keys(nearestBusStops);
- const values = Object.values(nearestBusStops);
-
-  getBusArrivals(keys,values);
+getBusArrivals(keys,values);
  
 
 
